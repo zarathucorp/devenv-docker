f2fc177ff7dd065cae6b6b3cd7fe88458334b432
changwoolim <limcw@zarathu.com>
2024-08-31 03:25:40 +0900

    Separate installing dependencies to files

---
+++ zarathu/dependencies/REMOTE.R
@@ -0,0 +1,9 @@
+# GitHub 등 CRAN 이외의 저장소
+remotes::install_github('jinseob2kim/jstable')
+remotes::install_github('jinseob2kim/jskm')
+remotes::install_github('emitanaka/shinycustomloader')
+remotes::install_github('Appsilon/shiny.i18n')
+remotes::install_github('metrumresearchgroup/sinew')
+remotes::install_github('jinseob2kim/jsmodule')
+remotes::install_github('yihui/xaringan')
+remotes::install_github('emitanaka/anicon')
