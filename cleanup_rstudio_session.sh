8dedc35d7c062a30e089f20d482a6be6adfcb7b7
Changwoo Lim <86377226+ChangwooLim@users.noreply.github.com>
2024-10-11 20:56:31 +0900

    Update cleanup_rstudio_session.sh

--- cleanup_rstudio_session.sh
+++ cleanup_rstudio_session.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-# Get the username from input
-read -p "유저 ID 입력: " USERNAME
+# Prompt for username
+read -p "유저 이름 입력: " USERNAME
 
-# RStudio 관련 폴더 삭제
-echo "$USERNAME의 RStudio 관련 폴더 삭제 시작"
-rm -rf /home/$USERNAME/.local/share/rstudio
-rm -rf /home/$USERNAME/.config/rstudio
+# Remove RStudio directories for the user
+rm -rf "/home/$USERNAME/.local/share/rstudio"
+rm -rf "/home/$USERNAME/.config/rstudio"
 
-echo "$USERNAME의 RStudio 관련 폴더 삭제 완료"
+# Get the list of active RStudio sessions for the user
+PIDs=$(rstudio-server active-sessions | tail -n +2 | awk -v user="$USERNAME" '$0 ~ user {print $1}')
 
-# 특정 유저의 세션 삭제
-kill_rstudio_session() {
-  local USERNAME="$1"
-
-  echo "$USERNAME의 세션 검색 중"
-  
-  sessions=$(rstudio-server active-sessions | grep "/usr/lib/rstudio-server/bin/rsession -u $USERNAME" | awk '{print $1}')
-
-  if [ -z "$sessions" ]; then
-    echo "$USERNAME의 세션 없음. 프로그램 종료"
-  else
-    echo "$USERNAME의 세션 $session 삭제 시작"
-    for session in $sessions; do
-      rstudio-server kill-session $session
-      echo "$USERNAME의 세션 $session 삭제 완료"
+if [ -z "$PIDs" ]; then
+    echo "유저 '$USERNAME'의 RStudio 세션이 없습니다."
+else
+    # Kill each active session for the user
+    for PID in $PIDs; do
+        echo "유저 '$USERNAME'의 RStudio 세션 $PID을 종료합니다."
+        rstudio-server kill-session "$PID"
     done
-  fi
-}
-
-kill_rstudio_session "$USERNAME"
+fi
