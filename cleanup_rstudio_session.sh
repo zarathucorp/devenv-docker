d2bde17627a5f35d6c23d834cc8e8aa41e02072a
Changwoo Lim <86377226+ChangwooLim@users.noreply.github.com>
2024-10-13 02:28:04 +0900

    Update cleanup_rstudio_session.sh

--- cleanup_rstudio_session.sh
+++ cleanup_rstudio_session.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Check if username is provided as a parameter
 if [ -z "$1" ]; then
     echo "사용법: $0 <유저이름>"
     exit 1
 fi
 
 USERNAME=$1
+USER_HOME="/home/$USERNAME"
 
-# Remove RStudio directories for the user
-rm -rf "/home/$USERNAME/.local/share/rstudio"
-rm -rf "/home/$USERNAME/.config/rstudio"
+if [ ! -d "$USER_HOME" ]; then
+    echo "유저 디렉토리 '$USER_HOME'가 존재하지 않습니다."
+    exit 2
+fi
 
-# Get the list of active RStudio sessions for the user
+rm -rf "$USER_HOME/.local/share/rstudio"
+rm -rf "$USER_HOME/.config/rstudio"
+
 PIDs=$(rstudio-server active-sessions | tail -n +2 | awk -v user="$USERNAME" '$0 ~ user {print $1}')
 
 if [ -z "$PIDs" ]; then
     echo "유저 '$USERNAME'의 RStudio 세션이 없습니다."
+    exit 3 
 else
-    # Kill each active session for the user
     for PID in $PIDs; do
         echo "유저 '$USERNAME'의 RStudio 세션 $PID을 종료합니다."
         rstudio-server kill-session "$PID"
     done
 fi
+
+exit 0
